26c6a5b20a0975ef9464ba8af7c9747c76f6d1f0
chenj5618 <76765168@qq.com>
2019-07-31 15:16:03 +0800

    代码提交

--- src/main/java/cn/zb/sms/dayuimpl/DayuSmsService.java
+++ src/main/java/cn/zb/sms/dayuimpl/DayuSmsService.java
@@ -8,11 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.alibaba.fastjson.JSONObject;
-import com.aliyuncs.CommonRequest;
 import com.aliyuncs.CommonResponse;
 import com.aliyuncs.DefaultAcsClient;
 import com.aliyuncs.IAcsClient;
-import com.aliyuncs.http.MethodType;
 import com.aliyuncs.profile.DefaultProfile;
 import com.aliyuncs.profile.IClientProfile;
 
